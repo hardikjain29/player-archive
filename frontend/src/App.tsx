1f28d3d54994270e1a9a6ed18ea08b90caae392e
Hardik <29hdkjain@gmail.com>
2021-05-19 09:52:43 +0530

    Show all prices

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,142 @@
-import React, { useState } from 'react';
-import SearchBox from './components/SearchBox/SearchBox';
-import EmptyState from './components/EmptyState/EmptyState';
-import PlayerCard from './components/PlayerCard/PlayerCard';
-import { PlayerType, ProfileType } from '../../types';
-import './App.css';
+// @ts-nocheck
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import orderTransactions from './orders.json';
+import './App.css'
+    let dataAnalytics = {};
+    const WazirX = () => {
+        const [tickers, setTickers] = useState({});
+        // const [dataAnalytics, setDataAnalytics] = useState({});
 
-const App: React.FC = () => {
-  const [profile, setProfile] = useState<ProfileType | null | undefined>(undefined);
-
-  const onSearch = async (id: string): Promise<void> => {
-    try {
-      const playerResponse = await fetch(`http://localhost:5000/player/${id}`);
-      const playerDetails: PlayerType = await playerResponse.json();
-
-      if (playerDetails.active) {
-        const profileResponse = await fetch(`http://localhost:5000/profile/${playerDetails['profile-id']}`);
-        const profileDetails: ProfileType = await profileResponse.json();
-
-        setProfile(profileDetails);
-      } else {
-        setProfile(null); // Player Not Active
-      }
-    } catch {
-      setProfile(null); // Player Not Found
-    }
-  } 
-
-  return (
-    <div className="wrapper">
-      <SearchBox onSearch={onSearch} />
-      <div className='results-wrapper'>
-        {
-          profile ? (
-            <PlayerCard profile={profile} />
-          ) : (
-            <EmptyState emptyType={profile} />
-          )
+        useEffect(() => {
+          const fetch = async () => {
+            const respone = await axios.get('https://api.wazirx.com/api/v2/tickers');
+            setTickers(respone.data);
+          }
+          fetch();
+        }, [])
+        const transactions = orderTransactions.filter((tra) => tra.state === 'done').map((tt) => ({
+          ...tt,
+          origin_volume: Number(tt.origin_volume),
+          avg_price: Number(tt.avg_price),
+        }));
+        const portfolioCoins = transactions.map((v) => v.market);
+        const portfolio = [...new Set(portfolioCoins)];
+    
+        const getAvgPrice = (coin) => {
+            let totalPurchaseAmount = 0;
+            let totalAmount = 0;
+            const coinTransactions = transactions.filter((transaction) => transaction.market === coin);
+            let buyTransactions = coinTransactions.filter((trans) => trans.kind === 'bid');
+            let newBuyTransactions = buyTransactions;
+            const sellTransactions = coinTransactions.filter((trans) => trans.kind === 'ask');
+            let totalSold = 0;
+            sellTransactions.map((trr) => {
+                totalSold = trr.origin_volume + totalSold;
+            });
+            buyTransactions.map((tr, index) => {
+                if (totalSold) {
+                    if (tr.origin_volume > totalSold) {
+                        const foundIndex = newBuyTransactions.findIndex((tra) => tra.id === tr.id);
+                        newBuyTransactions[foundIndex] = {
+                            ...tr,
+                            origin_volume: tr.origin_volume - totalSold,
+                        }
+                        totalSold = 0;
+                    }
+                    if (tr.origin_volume === totalSold) {
+                        newBuyTransactions = newBuyTransactions.filter((r, fi) => fi !== index);
+                        totalSold = 0;
+                    }
+                    if (tr.amount < totalSold) {
+                        newBuyTransactions = newBuyTransactions.filter((r, fii) => fii !== index);
+                        totalSold = totalSold - tr.origin_volume;
+                    }
+                }
+            });
+            // console.log(newBuyTransactions);
+            newBuyTransactions.map((transaction) => {
+                totalPurchaseAmount = (transaction.origin_volume * transaction.avg_price) + totalPurchaseAmount
+                totalAmount = transaction.origin_volume + totalAmount;
+            });
+            
+            dataAnalytics = {
+              ...dataAnalytics,
+              [coin]: {
+                totalPurchaseAmount,
+                totalAmount
+              }
+            };
+            
+            // transactions.map((transaction) => {
+            //     if (transaction.coin === coin) {
+            //         if (coin.type === 'buy') {
+            //             totalPurchaseAmount = (transaction.amount * transaction.price) + totalPurchaseAmount
+            //             totalAmount = transaction.amount + totalAmount;
+            //         } else {
+            //             totalPurchaseAmount = totalPurchaseAmount - (transaction.amount * transaction.price);
+            //             totalAmount = totalAmount - transaction.amount;
+            //         }
+            //     }
+            // });
+            return (totalPurchaseAmount/totalAmount) || 0;
         }
-      </div>
-    </div>
-  );
-}
+        console.log(Object.values(dataAnalytics));
+        const totalPL = Object.values(dataAnalytics).reduce((acc, curr) => curr.pl ? acc + curr.pl : acc, 0);
+        return (
+          <table className="stock-table">
+            <caption className="readable-hidden">Stock Information</caption>
+            <colgroup>
+              <col />
+              <col className="table-cell-1of8" span="7" />
+            </colgroup>
+            <thead>
+              <tr>
+                <th scope="col">Coin</th>
+                <th scope="col">Avg Cost</th>
+                <th scope="col">Current Price</th>
+                <th scope="col">Spent</th>
+                <th scope="col">Current Value</th>
+                <th scope="col">P&L</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                portfolio.map(coin => {
+                  const avgCost = getAvgPrice(coin);
+                  const spent = dataAnalytics[coin]?.totalPurchaseAmount;
+                  const current = tickers[coin]?.last * dataAnalytics[coin]?.totalAmount;
+                  const pl = current - spent;
+                  dataAnalytics = {
+                    ...dataAnalytics,
+                    [coin]: {
+                      ...dataAnalytics[coin],
+                      pl: pl,
+                    }
+                  }
+                  return (
+                    <tr>
+                      <th scope="row">{coin.replace('inr', '')}</th>
+                      <td>{avgCost}</td>
+                      <td>{tickers[coin]?.last}</td>
+                      <td>{spent}</td>
+                      <td>{current}</td>
+                      <td className={pl > 0 ? 'lower' : 'upper'}>{pl}</td>
+                    </tr>
+                  )
+                }) 
+              }
+               <tr>
+                  <th scope="row"></th>
+                  <td></td>
+                  <td></td>
+                  <td>{Object.values(dataAnalytics).reduce((acc, cur) => acc + cur.totalPurchaseAmount, 0)}</td>
+                  <td></td>
+                  <td className={totalPL > 0 ? 'lower' : 'upper'}>{totalPL}</td>
+                </tr>
+            </tbody>
+          </table>
+        )
+    };
 
-export default App;
+    export default WazirX;
